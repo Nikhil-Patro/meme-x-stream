d243404c9cec428f0c47e04860c385e8478fddee
Nikhil-Patro <nikhilpatro03@gmail.com>
2021-02-14 21:19:57 +0530

    9:19

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const devConfig = {
 // config for production
 
 const proConfig = {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: "postgres://fvlhjviyjkghew:f2e61c7257ac82506093b39bb2c619281f4d31b682f8f45b46c0c176a8be318b@ec2-35-174-118-71.compute-1.amazonaws.com:5432/d612556j5n030c",
 };
 
 const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
